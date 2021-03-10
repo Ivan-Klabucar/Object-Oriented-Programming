7486ad17d56d8011ffa256ef5964fa2f68b2351f
IvanKlabucar <ivan.klabucar@fer.hr>
2021-03-10 18:25:36 +0100

    prvi zadatak prvog laba

---
+++ OO_lab1/prvi.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include <stdlib.h> 
+
+typedef char const* (*PTRFUN)();
+
+char const* dogGreet(void){
+  return "vau!";
+}
+char const* dogMenu(void){
+  return "kuhanu govedinu";
+}
+char const* catGreet(void){
+  return "mijau!";
+}
+char const* catMenu(void){
+  return "konzerviranu tunjevinu";
+}
+
+PTRFUN DOG_VTABLE[2] = { dogGreet, dogMenu };
+PTRFUN CAT_VTABLE[2] = { catGreet, catMenu };
+
+struct Animal {
+    PTRFUN* vtf_;
+    char const * name_;
+};
+
+typedef struct Animal Animal;
+
+void constructDog(Animal* animal, char const* name) {
+    animal->vtf_ = DOG_VTABLE;
+    animal->name_ = name;
+}
+
+void constructCat(Animal* animal, char const* name) {
+    animal->vtf_ = CAT_VTABLE;
+    animal->name_ = name;
+}
+
+Animal* createDog(char const* name) {
+    Animal* animal = (Animal*)malloc(sizeof(Animal));
+    constructDog(animal, name);
+    return animal;
+}
+
+Animal* createCat(char const* name) {
+    Animal* animal = (Animal*)malloc(sizeof(Animal));
+    constructCat(animal, name);
+    return animal;
+}
+
+void animalPrintGreeting(Animal* animal) {
+    printf("%s pozdravlja: %s\n", animal->name_, animal->vtf_[0]());
+}
+
+void animalPrintMenu(Animal* animal) {
+    printf("%s voli %s\n", animal->name_, animal->vtf_[1]());
+}
+
+Animal* createNDogs(int n, char const* names[]) {
+    Animal* animal = (Animal*)malloc(n * sizeof(Animal));
+    for(int i = 0; i < n; i++) {
+        constructDog(animal + i, names[i]);
+    }
+    return animal;
+}
+
+void testAnimals(void){
+  struct Animal* p1=createDog("Hamlet");
+  struct Animal* p2=createCat("Ofelija");
+  struct Animal* p3=createDog("Polonije");
+
+  animalPrintGreeting(p1);
+  animalPrintGreeting(p2);
+  animalPrintGreeting(p3);
+
+  animalPrintMenu(p1);
+  animalPrintMenu(p2);
+  animalPrintMenu(p3);
+
+  free(p1); free(p2); free(p3);
+}
+
+int main(void) {
+    testAnimals();
+
+    // Animal na stogu
+    printf("\nAnimal na stogu:\n");
+    Animal dog1;
+    dog1.vtf_ = DOG_VTABLE;
+    dog1.name_ = "Kviki";
+    animalPrintGreeting(&dog1);
+    animalPrintMenu(&dog1);
+
+    // N pasa
+    printf("\n3 Psa na gomili:\n");
+    char const* names[3] = { "miki", "kiki", "sliki" };
+    Animal* dogs = createNDogs(3, names);
+    for(int i = 0; i < 3; i++) {
+        animalPrintGreeting(dogs + i);
+        animalPrintMenu(dogs + i);
+    }
+    return 0;
+}
